d997ba6faae5ce1776c4420a7a3450141dd4b14d
Philip Newborough <corenominal@corenominal.org>
2016-03-11 15:22:51 +0000

    Create plugin file

---
+++ iewp-content-snippets.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: IEWP Content Snippets
+ * Plugin URI: https://github.com/corenominal/iewp-content-snippets
+ * Description: A WordPress plugin for creating and displaying content snippets.
+ * Author: Philip Newborough
+ * Version: 0.0.1
+ * Author URI: https://corenominal.org
+ */
